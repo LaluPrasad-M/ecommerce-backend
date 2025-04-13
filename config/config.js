394b89e5c6e39fb96ecad297c65c6d5b6b1e7565
LaluPrasad-M <laluprasad.m@hingehealth.com>
2025-04-13 21:17:12 +0530

    Update MongoDB connection details in .env and config files; modify docker-compose.yml to comment out MongoDB service.

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 module.exports = {
   PORT: process.env.PORT || 3000,
-  MONGODB_URI: process.env.MONGODB_URI || "mongodb://localhost:27017/ecommerce",
+  MONGODB_URI: process.env.MONGODB_URI,
   NODE_ENV: process.env.NODE_ENV || "development",
   JWT_KEY: process.env.JWT_KEY || "secret"
 }; 
